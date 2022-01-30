47cf6c9e558333de7cb96e0654405fba5beae75b
Spencer Mattison <spenceramattison@gmail.com>
2022-01-30 12:59:13 +0900

    created hero and finished css styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Hero from "./components/Hero"
 
 export default function App() {
     return (
         <div className="container">
             <Navbar />
+            <Hero />
         </div>
     )
 }
